5369aa2af6f4ce176b3ea0bd8f928468c570b8e4
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2025-03-07 19:51:02 +0100

    fix: Views operations

--- tests/test_io.py
+++ tests/test_io.py
@@ -23,7 +23,28 @@ class TestIOVCFInfo:
         pb.read_vcf(vcf_big, info_fields=["AF", "vep"], thread_num=1).limit(1).collect()
     )
 
-    assert len(vcf_infos_mixed_cases) == 1
+    def test_count(self):
+        assert len(self.vcf_infos_mixed_cases) == 1
+
+
+class TestVCFViewsOperations:
+    def test_view(self):
+        vcf_big = "gs://gcp-public-data--gnomad/release/2.1.1/liftover_grch38/vcf/genomes/gnomad.genomes.r2.1.1.sites.liftover_grch38.vcf.bgz"
+        pb.register_vcf(vcf_big, "gnomad_big", info_fields=["AF", "vep"], thread_num=1)
+        pb.register_view(
+            "v_gnomad_big",
+            "SELECT chrom, start, end, split_part(vep, '|', 3) AS impact from gnomad_big where array_element(af,1)=0 and split_part(vep, '|', 3) in ('HIGH', 'MODERATE') limit 10",
+        )
+        vcf_sv = "gs://gcp-public-data--gnomad/release/4.1/genome_sv/gnomad.v4.1.sv.sites.vcf.gz"
+        pb.register_vcf(
+            vcf_sv, "gnomad_sv", thread_num=1, info_fields=["SVTYPE", "SVLEN"]
+        )
+        pb.register_view(
+            "v_gnomad_sv", "SELECT chrom, start, end FROM gnomad_sv limit 100"
+        )
+        assert len(pb.sql("SELECT * FROM v_gnomad_big").collect()) == 10
+        assert len(pb.nearest("v_gnomad_sv", "v_gnomad_big").collect()) == 100
+        assert len(pb.overlap("v_gnomad_sv", "v_gnomad_big").collect()) == 43
 
 
 class TestIOVCF:
