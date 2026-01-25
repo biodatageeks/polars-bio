a6d6ff7661bc24e575ebe72f5e3dcd0680e49cda
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2026-01-25 19:01:21 +0100

    chore: Prepare release 0.20.1 (#275)

--- polars_bio/__init__.py
+++ polars_bio/__init__.py
@@ -76,7 +76,7 @@ merge = range_operations.merge
 
 POLARS_BIO_MAX_THREADS = "datafusion.execution.target_partitions"
 
-__version__ = "0.20.0"
+__version__ = "0.20.1"
 __all__ = [
     "ctx",
     "FilterOp",
